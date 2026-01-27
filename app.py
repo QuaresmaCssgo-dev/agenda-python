67d3f7a2ca0fe9be76a0545d56fc0f7b14afa049
QuaresmaCssgo-dev <gustavoaaquaresma@hotmail.com>
2026-01-27 15:28:08 -0300

    feat: front=end com funcionalidade básica

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from models.tarefa import Tarefa
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html', titulo='Home')
+
+@app.route('/agenda', methods=['GET', 'POST'])
+def agenda():
+    tarefa = None
+
+    if request.method == 'POST':
+        titulo_tarefa = request.form['titulo-tarefa']
+        date_conclusao = request.form['data-conclusao']
+        tarefa = Tarefa(titulo_tarefa, date_conclusao)
+
+    return render_template('agenda.html', titulo='Agenda', tarefa=tarefa)
+
+
+@app.route('/ola')
+def ola_mundo():
+    return "Olá, Mundo!"
---
+++ models/tarefa.py
@@ -0,0 +1,4 @@
+class Tarefa: 
+    def __init__(self, titulo_tarefa, date_conclusao=None):
+        self.titulo_tarefa = titulo_tarefa
+        self.date_conclusao = date_conclusao
